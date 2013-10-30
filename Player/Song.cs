9903faff178d735a8ec6784146503451ad548be9
am636e <bnazariy@gmail.com>
2013-10-30 15:44:34 +0100

    Working on playlist. Song class Added

---
+++ Player/Song.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Wave;
+
+
+namespace Player
+{
+    class Song
+    {
+        private string _path;
+        private WaveStream _stream;
+
+        public string Path { get { return _path; } }
+        public string Extension { get { return _path.Substring(_path.LastIndexOf('.')); } }
+
+        public Song()
+        { }
+
+        public Song(string path)
+        {
+            _path = path;
+            _stream = GetStream();
+        }
+
+        public WaveStream GetStream()
+        {
+            switch(Extension)
+            {
+                case ".mp3":
+                    {
+                        return new Mp3FileReader(_path);
+                    }
+                case ".wav":
+                    {
+                        return new WaveFileReader(_path);
+                    }
+                default:
+                    {
+                        throw new OperationCanceledException("Not Supported Extension");
+                    }
+            }
+        }
+    }
+}
